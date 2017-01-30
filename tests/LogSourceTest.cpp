2a9eca0270033be09d8dd38f5431a5d6a6dc1b03
takehiro yashiro <tkyashiro@haketrio.sakura.ne.jp>
2017-01-30 23:27:34 +0900

    Modified CMakeLists.txt to support testing, added empty test

---
+++ tests/LogSourceTest.cpp
@@ -0,0 +1,14 @@
+#include <QTest>
+
+class LogSourceTest : public QObject
+{
+	Q_OBJECT
+public:
+
+private slots:
+	void test(){ QVERIFY(true); }
+};
+
+QTEST_APPLESS_MAIN(LogSourceTest)
+#include "LogSourceTest.moc"
+
